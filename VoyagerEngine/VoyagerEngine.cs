61adf78a494019cea688e4b65322a4cdf1e71185
voyagergames-charlespark <107326300+voyagergames-charlespark@users.noreply.github.com>
2023-09-14 11:58:41 -0700

    Initial commit

---
+++ VoyagerEngine/VoyagerEngine.cs
@@ -0,0 +1,32 @@
+﻿using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+internal class Program
+{
+    private static IWindow _window;
+    private static IInputContext _input;
+    private static void Main(string[] args)
+    {
+        WindowOptions options = WindowOptions.Default with
+        {
+            Size = new Vector2D<int>(800, 600),
+            Title = "Mystery Dungeon"
+        };
+        _window = Window.Create(options);
+
+        _window.Load += OnLoad;
+        _window.Update += OnUpdate;
+        _window.Render += OnRender;
+
+        _window.Run();
+        //_window.Close();
+    }
+    private static void OnLoad() {
+        _input = _window.CreateInput();
+    }
+
+    private static void OnUpdate(double deltaTime) { }
+
+    private static void OnRender(double deltaTime) { }
+
+}
